76587e1b71f8da46988e440819d9a107eee64a6e
Jonathan Han <jonweshan@gmail.com>
2022-10-10 15:49:03 -0700

    Add specifications and methods for Account class
    Add multiple methods including a constructor, getter, setters. Also add specifications for methods for design clarity.

--- src/main/model/Account.java
+++ src/main/model/Account.java
@@ -1,4 +1,57 @@
 package model;
 
 public class Account {
+
+    /*
+     * REQUIRES: accountName and accountPassword have non-zero length
+     * EFFECTS: constructs an Account object with specified fields
+     */
+    public void Account(String name, String username, String password) {
+        //stub
+    }
+
+    /*
+     * REQUIRES: name must have non-zero length
+     * MODIFIES: this
+     * EFFECTS: changes the account's associated name
+     */
+    public void setName(String name) {
+        //stub
+    }
+
+    /*
+     * REQUIRES: username must have non-zero length
+     * MODIFIES: this
+     * EFFECTS: changes the account's associated username
+     */
+    public void setUsername(String username) {
+        //stub
+    }
+
+    /*
+     * REQUIRES: password must have non-zero length
+     * MODIFIES: this
+     * EFFECTS: changes the account's associated password
+     */
+    public void setPassword(string password) {
+        //stub
+    }
+
+    public String getName() {
+        return null;    //stub
+    }
+
+    public String getUsername() {
+        return null;    //stub
+    }
+
+    public String getPassword() {
+        return null;    //stub
+    }
 }
+
+/*
+ * REQUIRES:
+ * MODIFIES:
+ * EFFECTS:
+ */

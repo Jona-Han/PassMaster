50d119c3a7ee309f13290b0396e7e4aba72af483
Jonathan Han <jonweshan@gmail.com>
2022-10-14 11:07:18 -0700

    Add setup to ListOfAccountsTest

--- src/test/model/ListOfAccountsTest.java
+++ src/test/model/ListOfAccountsTest.java
@@ -1,4 +1,25 @@
 package model;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
 public class ListOfAccountsTest {
+    ListOfAccounts list;
+    Account a;
+    Account b;
+    Account c;
+
+    @BeforeEach
+    private void setUp() {
+        list = new ListOfAccounts();
+        a = new Account("a", "usera", "passa");
+        b = new Account("a", "usera", "passa");
+        c = new Account("a", "usera", "passa");
+
+        list.add(a);
+        list.add(b);
+        list.add(c);
+    }
+
+
 }

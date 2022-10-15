16e8c6c7b195a98040991c6fa8ce25161e7bfb18
Jonathan Han <jonweshan@gmail.com>
2022-10-14 18:59:57 -0700

    Create get and remove test methods

--- src/main/model/ListOfAccounts.java
+++ src/main/model/ListOfAccounts.java
@@ -12,42 +12,68 @@ public class ListOfAccounts {
     public ListOfAccounts() {
         accounts = new ArrayList<>();
     }
+    /*
+     * EFFECTS: Constructs a new ListOfAccounts with an account
+     */
+    public ListOfAccounts(Account account) {
+        accounts = new ArrayList<>();
+        accounts.add(account);
+    }
+
+    /*
+     * REQUIRES: account is non-null and not already in the ListOfAccounts
+     * MODIFIES: this
+     * EFFECTS: adds an account to the ListOfAccounts, returns true if successfully added
+     */
+    public boolean add(Account account) {
+
+    }
 
     /*
      * REQUIRES: account is non-null
      * MODIFIES: this
-     * EFFECTS: adds an account to the ListOfAccounts
+     * EFFECTS: removes an Account account form ListOfAccounts, returns true if successfully removed
      */
-    public void add(Account account) {}
-
-    /*
-     * REQUIRES: account is non-null
-     * MODIFIES: this
-     * EFFECTS: removes an Account account form ListOfAccounts
-     */
-    public void remove(Account account) {}
+    public boolean remove(Account account) {
+        return false;
+    }
 
     /*
      * REQUIRES: index >= 0
      * MODIFIES: this
      * EFFECTS: removes an account at the specified index
      */
-    public void remove(int index) {}
+    public boolean remove(int index) {
+        return false;
+    }
 
     /*
      * REQUIRES: accounts is non-null
      * EFFECTS: checks if there's an Account account in the ListOfAccounts and returns true if there is
      */
-    public boolean contains(Account account) {}
+    public boolean contains(Account account) {
+        return true;
+    }
 
     /*
      * EFFECTS: returns the size of the ListOfAccounts
      */
-    public int size() {}
+    public int size() {
+        return 0;
+    }
 
     /*
      * REQUIRES: index >= 0
      * EFFECTS: returns an Account at the specified index
      */
-    public Account get(int index) {}
+    public Account get(int index) {
+        return null;
+    }
+
+    /*
+     * EFFECTS: returns the inner list
+     */
+    public List<Account> getInnerList() {
+        return accounts;
+    }
 }
--- src/test/model/ListOfAccountsTest.java
+++ src/test/model/ListOfAccountsTest.java
@@ -3,24 +3,103 @@ package model;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.*;
+
 public class ListOfAccountsTest {
-    ListOfAccounts list;
+    ListOfAccounts testListOfAccounts;
+    List<Account> actualInnerList;
     Account a;
     Account b;
     Account c;
 
     @BeforeEach
-    private void setUp() {
-        list = new ListOfAccounts();
+    void setUp() {
         a = new Account("a", "usera", "passa");
         b = new Account("b", "userb", "passb");
         c = new Account("c", "userc", "passc");
 
-        list.add(a);
-        list.add(b);
-        list.add(c);
+        testListOfAccounts = new ListOfAccounts(a);
+        List<Account> actualInnerList = testListOfAccounts.getInnerList();
     }
 
+    @Test
+    void testEmptyConstructor() {
+        testListOfAccounts = new ListOfAccounts();
+        List<Account> actualList = testListOfAccounts.getInnerList();
+        assertEquals(0, actualList.size());
+    }
+
+    @Test
+    void testConstructor() {
+        testListOfAccounts = new ListOfAccounts(a);
+        List<Account> actualInnerList = testListOfAccounts.getInnerList();
+        assertEquals(1, actualInnerList.size());
+    }
+
+    @Test
+    void testAddOneTime() {
+        assertTrue(testListOfAccounts.add(b));
+        assertEquals(2, actualInnerList.size());
+        assertTrue(actualInnerList.contains(a));
+        assertTrue(actualInnerList.contains(b));
+    }
+
+    @Test
+    void testAddSameObject() {
+        assertFalse(testListOfAccounts.add(a));
+        assertEquals(1, actualInnerList.size());
+        assertTrue(actualInnerList.contains(a));
+    }
+
+    @Test
+    void testAddMultipleTimes() {
+        testListOfAccounts.add(b);
+        testListOfAccounts.add(c);
+
+        assertEquals(3, actualInnerList.size());
+        assertTrue(actualInnerList.contains(a));
+        assertTrue(actualInnerList.contains(b));
+        assertTrue(actualInnerList.contains(c));
+    }
+
+    @Test
+    void testRemoveObjectParameterSucceed() {
+        assertTrue(testListOfAccounts.remove(a));
+        assertEquals(0, actualInnerList.size());
+    }
+
+    @Test
+    void testRemoveObjectParameterFail() {
+        assertFalse(testListOfAccounts.remove(b));
+        assertEquals(1, actualInnerList.size());
+    }
+
+    @Test
+    void testRemoveIntegerParameterSucceed() {
+        assertTrue(testListOfAccounts.remove(0));
+        assertEquals(0, actualInnerList.size());
+    }
+
+    @Test
+    void testRemoveIntegerParameterFail() {
+        assertFalse(testListOfAccounts.remove(1));
+        assertEquals(1, actualInnerList.size());
+    }
+
+    @Test
+    void testGet() {
+        assertEquals(a, actualInnerList.get(0));
+    }
+
+    @Test
+    void testGetWithMultipleElementsInList() {
+        testListOfAccounts.add(b);
+        testListOfAccounts.add(c);
+        assertEquals(b, testListOfAccounts.get(1));
+        assertEquals(c, testListOfAccounts.get(2));
+    }
 
 
 }

32bd5df7c8ec4b5fbfce857eafb1365a74389981
Jonathan Han <jonweshan@gmail.com>
2022-10-16 23:57:39 -0700

    Write more detailed documentation and make specifications more clear

--- src/main/ui/PasswordManagerApp.java
+++ src/main/ui/PasswordManagerApp.java
@@ -22,14 +22,15 @@ public class PasswordManagerApp {
      */
     private void runLoginProcess() {
         String password = "password"; //Won't be hard-coded in the future
-        boolean running = true;
+        boolean passwordCorrect = false;
 
-        while (running) {
+        while (!passwordCorrect) {
             System.out.println("Please enter your password: ");
             String input = getUserInputString();
 
+            //If password is correct, start manager
             if (Objects.equals(input, password)) {
-                running = false;
+                passwordCorrect = true;
                 runManagerProcess();
             }
         }
@@ -45,6 +46,7 @@ public class PasswordManagerApp {
         boolean running = true;
         String userInput;
 
+        //Main loop
         while (running) {
             displayAllAccounts();
             displayCommandMenu();
@@ -76,13 +78,15 @@ public class PasswordManagerApp {
     }
 
     /*
-     * EFFECTS: Displays a list of all stored accounts to user%-1s
+     * EFFECTS: Displays a list of all stored accounts to user
      */
     private void displayAllAccounts() {
         System.out.println("\n_____LIST OF ACCOUNTS_____");
 
         //SOURCE: https://docs.oracle.com/javase/7/docs/api/java/util/Formatter.html
         System.out.format("%-10s %-1s", "Number:", "Account Name:");
+
+        //Print out all the names of accounts
         for (int index = 0; index < accounts.size(); index++) {
             System.out.format("%n %-10s", index + 1);
             printArray(accounts.get(index).getName());
@@ -97,6 +101,8 @@ public class PasswordManagerApp {
     private void processCommand(String command) {
         Account accountToManage;
         command = command.toLowerCase();
+
+        // v for view/edit, a for add account, d for remove account
         switch (command) {
             case "v":
                 accountToManage = getAccountFromAccounts();
@@ -118,16 +124,18 @@ public class PasswordManagerApp {
     }
 
     /*
-     * EFFECTS: Prompts the user for the number of the desired account and returns its index in accounts
+     * REQUIRES: accounts.size() > 0, 0 <= userInput/indexInAccounts < accounts.size()
+     * EFFECTS: Prompts the user for the number of the desired account and returns the account at that position
      */
     private Account getAccountFromAccounts() {
         System.out.println("\nWhich account number?");
         String userInput = getUserInputString();
-        int indexInAccounts = Integer.parseInt(userInput) - 1;
+        int indexInAccounts = Integer.parseInt(userInput) - 1;  //Converts user input into an integer
         return accounts.get(indexInAccounts);
     }
 
     /*
+     * REQUIRES: accounts.size() > 0
      * EFFECTS: Displays information about a specific account stored in the manager
      */
     private void viewSpecificAccountInformation(Account account) {
@@ -143,14 +151,15 @@ public class PasswordManagerApp {
 
     /*
      * REQUIRES: account must be non-null
-     * MODIFIES: this, CollectionOfAccounts, account
-     * EFFECTS: Displays information about a specific account stored in the manager
+     * MODIFIES: this, CollectionOfAccounts, Account
+     * EFFECTS: Asks user and enters editing mode if yes answer is given
      */
     private void promptUserToEnterEditingMode(Account account) {
         //Ask user if they would like to edit the account
         System.out.println("\nWould you like to edit this account? (y/n)");
         String userInput = getUserInputString();
 
+        //Enter editing mode if yes
         if (userInput.equals("y") || userInput.equals("Y")) {
             editAccount(account);
         }
@@ -162,13 +171,14 @@ public class PasswordManagerApp {
      * MODIFIES: this, CollectionOfAccounts
      * EFFECTS: Adds an account to the manager
      */
-    protected void addAccount(Account account) {
+    private void addAccount(Account account) {
         accounts.add(account);
         System.out.println("\nAccount successfully added!");
     }
 
     /*
-     * EFFECTS: Prompts user for new account info, instantiates new account, and returns it
+     * REQUIRES: name must have non-zero length
+     * EFFECTS: Asks user for new account info, instantiates new account, and returns it
      */
     private Account getNewAccountInfo() {
         System.out.println("\nWhat would you like to name the new account?");
@@ -184,7 +194,7 @@ public class PasswordManagerApp {
     }
 
     /*
-     * REQUIRES: account is non-null
+     * REQUIRES: account is non-null and must be in accounts
      * MODIFIES: this, CollectionOfAccounts
      * EFFECTS: Removes an account from the password manager
      */
@@ -194,30 +204,30 @@ public class PasswordManagerApp {
     }
 
     /*
-     * REQUIRES: account is non-null
+     * REQUIRES: account is non-null and must be in accounts
      * MODIFIES: this, CollectionOfAccounts, Account
-     * EFFECTS: Edits account information
+     * EFFECTS: Asks user what they would like to edit and calls appropriate method
      */
     private void editAccount(Account account) {
-        //Continue editing until user exits editing mode
         boolean editing = true;
+        //Continue editing until user exits editing mode
         while (editing) {
+            //Prompt user and get input
             viewSpecificAccountInformation(account);
             System.out.println("\nPlease enter the field you would like to change:\na: Name\t\tb: Username"
                     + "\t\tc: Password\t\td: Exit editing mode");
             String userInput = getUserInputString();
-            //Change name if "a", change username if "b", change password if "c"
+            userInput = userInput.toLowerCase();
+
+            //Change name if "a", change username if "b", change password if "c", exit editing mode if anything else
             switch (userInput) {
                 case "a":
-                case "A":
                     changeName(account);
                     break;
                 case "b":
-                case "B":
                     changeUsername(account);
                     break;
                 case "c":
-                case "C":
                     changePassword(account);
                     break;
                 default:
@@ -227,11 +237,11 @@ public class PasswordManagerApp {
     }
 
     /*
-     * REQUIRES: account must not be null and must be in the list of accounts
-     * MODIFIES: this, account
+     * REQUIRES: account must be non-null and must be in accounts
+     * MODIFIES: this, CollectionOfAccounts Account
      * EFFECTS: Changes name for a specified account
      */
-    protected void changeName(Account account) {
+    private void changeName(Account account) {
         //Print out prompt and get user input
         System.out.print("\nWhat would you like to change the name to?\nCurrently: ");
         printArray(account.getName());
@@ -244,11 +254,11 @@ public class PasswordManagerApp {
     }
 
     /*
-     * REQUIRES: account must not be null and must be in the list of accounts
+     * REQUIRES: account must be non-null and must be in accounts
      * MODIFIES: this, account
      * EFFECTS: Changes username for a specified account
      */
-    protected void changeUsername(Account account) {
+    private void changeUsername(Account account) {
         //Print out prompt and get user input
         System.out.print("\nWhat would you like to change the username to?\nCurrently: ");
         printArray(account.getUsername());
@@ -261,16 +271,15 @@ public class PasswordManagerApp {
     }
 
     /*
-     * REQUIRES: account must not be null and must be in the list of accounts
+     * REQUIRES: account must be non-null and must be in the list of accounts
      * MODIFIES: this, account
      * EFFECTS: Changes password for a specified account
      */
-    protected void changePassword(Account account) {
+    private void changePassword(Account account) {
         //Print out prompt and get user input
         System.out.print("\nWhat would you like to change the password to?\nCurrently: ");
         printArray(account.getPassword());
         System.out.println();
-
         char[] userInput = getUserInput();
 
         //Change password
